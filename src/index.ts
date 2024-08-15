09896b6c2359c8002cea105a1529becea24d2c82
Alexey Kalmakov (rndD) <st00nsa@gmail.com>
2024-08-16 00:15:13 +0200

    + little js

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,43 @@
-import { drawEngine } from './core/draw-engine';
-import { menuState } from './game-states/menu.state';
-import { createGameStateMachine, gameStateMachine } from './game-state-machine';
-import { controls } from '@/core/controls';
 
-createGameStateMachine(menuState);
+import {drawTextScreen, mainCanvasSize, engineInit} from 'littlejsengine';
 
-let previousTime = 0;
-const interval = 1000 / 60;
 
-(function draw(currentTime: number) {
-  const delta = currentTime - previousTime;
+///////////////////////////////////////////////////////////////////////////////
+function gameInit()
+{
+    // called once after the engine starts up
+    // setup the game
+}
 
-  if (delta >= interval) {
-    previousTime = currentTime - (delta % interval);
+///////////////////////////////////////////////////////////////////////////////
+function gameUpdate()
+{
+    // called every frame at 60 frames per second
+    // handle input and update the game state
+}
 
-    controls.queryController();
-    drawEngine.context.clearRect(0, 0, drawEngine.canvasWidth, drawEngine.canvasHeight);
-    // Although the game is currently set at 60fps, the state machine accepts a time passed to onUpdate
-    // If you'd like to unlock the framerate, you can instead use an interval passed to onUpdate to 
-    // adjust your physics so they are consistent across all frame rates.
-    // If you do not limit your fps or account for the interval your game will be far too fast or far too 
-    // slow for anyone with a different refresh rate than you.
-    gameStateMachine.getState().onUpdate(delta);
-  }
-  requestAnimationFrame(draw);
-})(0);
+///////////////////////////////////////////////////////////////////////////////
+function gameUpdatePost()
+{
+    // called after physics and objects are updated
+    // setup camera and prepare for render
+}
+
+///////////////////////////////////////////////////////////////////////////////
+function gameRender()
+{
+    // called before objects are rendered
+    // draw any background effects that appear behind objects
+}
+
+///////////////////////////////////////////////////////////////////////////////
+function gameRenderPost()
+{
+    // called after objects are rendered
+    // draw effects or hud that appear above all objects
+    drawTextScreen('Hello World!', mainCanvasSize.scale(.5), 80);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Startup LittleJS Engine
+engineInit(gameInit, gameUpdate, gameUpdatePost, gameRender, gameRenderPost);
